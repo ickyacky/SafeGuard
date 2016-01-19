437de952035c451190f10254f370bb49f02a36b2
Mike Botsko <botsko@gmail.com>
2016-01-18 18:51:25 -0800

    Checkstyle fixes

--- src/main/java/com/helion3/safeguard/zones/Zone.java
+++ src/main/java/com/helion3/safeguard/zones/Zone.java
@@ -158,6 +158,14 @@ public class Zone implements DataSerializable {
     }
 
     @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((uuid == null) ? 0 : uuid.hashCode());
+        return result;
+    }
+
+    @Override
     public boolean equals(Object object) {
         if (object instanceof Zone) {
             Zone zone = (Zone) object;
